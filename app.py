3b6b47b9d7757107d27f4a69e672e708812564e3
macieljure <77366478+WhatCameTo@users.noreply.github.com>
2025-09-08 10:16:58 -0600

    commas correction

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ number_of_trials = st.slider('Number of Attempts?', 1, 1000, 10)
 start_button = st.button('Execute')
 
 if start_button:
-	st.write(f'Experiment with {number_of_trials}' ongoing trials.)
+	st.write(f'Experiment with {number_of_trials} ongoing trials.')
 
 st.write('This app is not functional yet')
 
