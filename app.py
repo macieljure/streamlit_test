7b6213626c2094c6f3ff037ee3c09bb597d267e4
macieljure <77366478+WhatCameTo@users.noreply.github.com>
2025-09-08 10:08:27 -0600

    Adding slider, and program button

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 
 st.header('Throw a coin')
 
+number_of_trials = st.slider('Number of Attempts?', 1, 1000, 10)
+
+start_button = st.button('Execute')
+
+if start_button:
+	st.write(f'Experiment with {number_of_trials}' ongoing trials.)
+
 st.write('This app is not functional yet')
 
 
