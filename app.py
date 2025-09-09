694ce853724212d87348c53cadddea756be10b5d
macieljure <77366478+WhatCameTo@users.noreply.github.com>
2025-09-09 09:00:58 -0600

    typo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if 'experiment_no' not in st.session_state:
 	st.session_state['experiment_no'] = 0
 
 if 'df_experiment_results' not in st.session_state:
-	st.session_state['df_experiment_results'] = ps.DataFrame(columns=['no', 'iterations', 'mean'])
+	st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
 
 
 
